a56ab23e576ffa23888c6fe422687d921326f056
thereallucas98 <lucas1998david@gmail.com>
2022-07-25 20:32:12 -0300

    Carregamento da Splash && Gerando Android App Bundle

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
+
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
